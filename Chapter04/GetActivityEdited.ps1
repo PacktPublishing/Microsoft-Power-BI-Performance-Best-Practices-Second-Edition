9649f81197639c10f2c68d281a50bf89224337e4
SQLThomasMVP <34689594+SQLThomasMVP@users.noreply.github.com>
2024-06-30 20:29:02 -0500

    Add files via upload

---
+++ Chapter04/GetActivityEdited.ps1
@@ -0,0 +1,27 @@
+﻿$AppId = 'appid guid'
+$AppSecret = '[secret value]'
+$Tenant = '<tenant guid>'
+#Clear
+# Prerequisits - install Powershell Power  Bi modules
+
+Install-Module -Name MicrosoftPowerBIMgmt.Profile -Force
+# Install module
+Install-Module -Name MicrosoftPowerBIMgmt
+
+#Pass credentials
+$PWord = ConvertTo-SecureString -String $AppSecret -AsPlainText -Force
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $AppId, $PWord
+Connect-PowerBIServiceAccount -ServicePrincipal -Credential $Credential -Tenant d289a47a-3ec6-436b-8040-85591cd46d7b
+
+ 
+$Dates = Get-Date((get-date ).AddDays(-1))  -Format "yyyy-MM-dd"
+$StartDate = $Dates + 'T00:00:00'
+$EndDate = $Dates + 'T23:59:59'
+ 
+ 
+$json = Get-PowerBIActivityEvent -StartDateTime $StartDate -EndDateTime $EndDate | ConvertFrom-Json
+$activity = $json | Select Id, CreationTime,Workload, UserId, Activity, ItemName, WorkSpaceName, DatasetName, ReportName, WorkspaceId, ObjectId, DatasetId, ReportId, ReportType ,DistributionMethod, ConsumptionMethod
+ 
+$activity
+    Get-PowerBIActivityEvent -StartDateTime $StartDate -EndDateTime $EndDate -ResultType JsonString | 
+    Out-File -FilePath "c:\temp\AuditLog_$(Get-Date -Date $EndDate -Format yyyyMMdd).json"
